0021301ce85e4bbd824fa95a21d8069737666c0c
Jorge Ramirez <ing.jorgeivanramirezh@gmail.com>
2022-03-20 18:21:46 +0100

    github action

---
+++ index.js
@@ -0,0 +1,5 @@
+function perro(nombre, raza) {
+  return `${nombre} es un perro de raza ${raza}`
+}
+
+module.exports = perro
---
+++ index.test.js
@@ -0,0 +1,8 @@
+const perro = require('./index')
+
+describe('index', () => {
+  it('should say the breed of the dog', () => {
+    const result = perro('Firulais', 'Bulldog')
+    expect(result).toBe('Firulais es un perro de raza Bulldog')
+  })
+})
