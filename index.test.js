bb6b3de7169d5426bb604de8448a7bc841008262
Jorge Ramirez <ing.jorgeivanramirezh@gmail.com>
2022-03-20 18:41:42 +0100

    gato

--- index.test.js
+++ index.test.js
@@ -3,6 +3,6 @@ const perro = require('./index')
 describe('index', () => {
   it('should say the breed of the dog', () => {
     const result = perro('Firulais', 'Bulldog')
-    expect(result).toBe('Firulais es un perro de raza Bulldog')
+    expect(result).toBe('Firulais es un perro de raza Bulldog 🐶')
   })
 })
